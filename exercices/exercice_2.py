81e0cda519ac3b964840cfe7ff7bc7c6ae0c740d
gabriel181414 <gabrielramirezsuport@gmail.com>
2023-02-15 17:03:13 -0400

    first commit

---
+++ exercices/exercice_2.py
@@ -0,0 +1,14 @@
+# Operadores matemáticos
+
+print(2 + 2)
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+print(8 / 5)
+print(80 + 20)
+print(17 / 3)
+print(17 // 3)
+print(17 % 3)
+print(5 * 3 + 2)
+print(5 ** 2)
+print(2 ** 7)
+
---
+++ exercices/exercice_3.py
@@ -0,0 +1,3 @@
+ancho = 20
+largo = 5 * 9
+print(ancho * largo)
